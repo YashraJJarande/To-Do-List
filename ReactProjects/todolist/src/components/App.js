6e21884e0fecbdfc8c196a65448a7660b223a00d
YashraJJarande <yashrajjarande54455445@gmail.com>
2023-10-06 12:06:44 +0530

    First commit: 	new file:   ReactProjects/todolist/README.md 	new file:   ReactProjects/todolist/package-lock.json 	new file:   ReactProjects/todolist/package.json 	new file:   ReactProjects/todolist/public/index.html 	new file:   ReactProjects/todolist/public/styles.css 	new file:   ReactProjects/todolist/src/components/App.js 	new file:   ReactProjects/todolist/src/components/InputArea.js 	new file:   ReactProjects/todolist/src/components/Todaoitems.js 	new file:   ReactProjects/todolist/src/index.js

---
+++ ReactProjects/todolist/src/components/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Todaoitems from "./Todaoitems";
+import InputArea from "./InputArea";
+
+function App() {
+const [newitem , setnewitem] = useState([]);
+function StoreArray(text){
+    setnewitem( preValue => {
+        return [ ...preValue,
+            text];
+    });
+   }
+function deleteitem(id){ 
+    
+    setnewitem(Previtem =>
+    {
+        return Previtem.filter((item,index) => {
+           
+            return !(index === id)
+        }     
+        );            
+    })
+ }
+ return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea 
+        onAdd = {StoreArray}
+      />
+      <div>
+        <ul>
+          { newitem.map( (items,index) => (
+            <Todaoitems 
+            key = {index}
+            text = {items} 
+            onClicked= {deleteitem}
+            id={index}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );          }
+export default App;
